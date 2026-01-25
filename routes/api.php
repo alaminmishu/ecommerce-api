f10a8dbc01f4607538b7697078f9246fcb3246c8
alaminmishu <aaamishucse@gmail.com>
2026-01-25 15:32:27 +0600

    Add product API routes (public and protected)

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\Api\V1\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('v1')->group(function () {
+    // Public routes
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('products/{uid}', [ProductController::class, 'show']);
+
+    // Protected routes
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('products', [ProductController::class, 'store']);
+        Route::put('products/{product}', [ProductController::class, 'update']);
+        Route::delete('products/{product}', [ProductController::class, 'destroy']);
+    });
+});
