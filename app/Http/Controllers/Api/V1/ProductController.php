18e2bad8f272a5be537333d13e53a8ab772599d4
alaminmishu <aaamishucse@gmail.com>
2026-01-25 15:26:49 +0600

    Add Product API controller with index and show methods

---
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $products = Product::query()
+            ->with(['primaryImage', 'defaultVariant'])
+            ->active()
+            ->published()
+            ->orderBy('sort_position')
+            ->paginate($request->get('per_page', 15));
+
+        return response()->json($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $uid)
+    {
+        $product = Product::where('uid', $uid)
+            ->with(['images', 'variants', 'categories'])
+            ->active()
+            ->published()
+            ->firstOrFail();
+
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(product $product)
+    {
+        //
+    }
+}
